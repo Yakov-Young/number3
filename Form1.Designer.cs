e3b0e86f31ed1ef05ef2f8d65a904c4c15ebfa12
Yakov-Young <suburbanms@gmail.com>
2022-03-10 00:56:28 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сгенерироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1076, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem,
+            this.рассчитатьToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.сгенерироватьToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 587);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1076, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.excelToolStripMenuItem.Text = "Excel";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // рассчитатьToolStripMenuItem
+            // 
+            this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+            this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.рассчитатьToolStripMenuItem.Text = "Рассчитать";
+            this.рассчитатьToolStripMenuItem.Click += new System.EventHandler(this.рассчитатьToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // сгенерироватьToolStripMenuItem
+            // 
+            this.сгенерироватьToolStripMenuItem.Name = "сгенерироватьToolStripMenuItem";
+            this.сгенерироватьToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.сгенерироватьToolStripMenuItem.Text = "Сгенерировать";
+            this.сгенерироватьToolStripMenuItem.Click += new System.EventHandler(this.сгенерироватьToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.x,
+            this.y});
+            this.dataGridView1.Location = new System.Drawing.Point(136, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 270);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // x
+            // 
+            this.x.HeaderText = "x";
+            this.x.Name = "x";
+            // 
+            // y
+            // 
+            this.y.HeaderText = "y";
+            this.y.Name = "y";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "n=1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Функция: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Функция: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "n=2";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(386, 27);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(654, 299);
+            this.zedGraphControl1.TabIndex = 9;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Коэффициент детерминации: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(469, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(159, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Коэффициент детерминации: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 306);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Количетсво строк =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 611);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolStripMenuItem сгенерироватьToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Microsoft.Office.Interop.Excel;
+using ZedGraph;
+using org.mariuszgromada.math.mxparser;
+using System.Threading;
+
+namespace lab_3
+{
+    public partial class Form1 : Form
+    {
+        double maximum = -1 * Math.Pow(2, 69);
+        double minimum = Math.Pow(2, 69);
+
+        List<double> listX = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = String.Empty;
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res != DialogResult.OK) return;
+
+            try
+            {
+                dataGridView1.Rows.Clear();
+                Application AppExcel = new Application();
+                Workbook workSpaceExcel = AppExcel.Workbooks.Open(openFileDialog1.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing); //открыть файл
+                Worksheet sheetExcel = (Worksheet)workSpaceExcel.Sheets[1];
+                if (sheetExcel.Rows.CurrentRegion.EntireRow.Count == 1)
+                {
+                    MessageBox.Show("В Excel-файле только одна строка");
+                }
+                else
+                {
+                    dataGridView1.Rows.Clear();
+                    var lastCell = sheetExcel.Cells.SpecialCells(XlCellType.xlCellTypeLastCell);  //...SpecialCells(XlCellType.xlCellTypeLastCell) - возвращает диапазон ячеек
+                    string x = String.Empty;
+                    string y = String.Empty;
+                    for (int i = 0; i < lastCell.Row; i++)
+                    {
+                        x = sheetExcel.Cells[i + 1, 1].Text.ToString();
+                        y = sheetExcel.Cells[i + 1, 2].Text.ToString();
+                        if (x.Trim() != String.Empty && y.Trim() != String.Empty) {
+                            dataGridView1.Rows.Add();
+                            dataGridView1[0, i].Value = x;
+                            dataGridView1[1, i].Value = y;
+                        }
+                    }
+                }
+                workSpaceExcel.Close(false, Type.Missing, Type.Missing);
+                AppExcel.Quit();
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка при загрузке файла!");
+            }
+        }
+
+        private void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.AllowUserToAddRows = false;
+          
+            GraphPane graph = zedGraphControl1.GraphPane;
+            graph.CurveList.Clear();
+            PointPairList list_points = new PointPairList();
+            List<double> values = new List<double>();
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                list_points.Add(Convert.ToDouble(dataGridView1[0, i].Value), Convert.ToDouble(dataGridView1[1, i].Value));
+            }
+            dataGridView1.AllowUserToAddRows = true;
+
+            LineItem line = graph.AddCurve("Точки", list_points, Color.Green, SymbolType.Circle);
+            line.Line.IsVisible = false;
+
+
+            Thread graphic = new Thread(new ThreadStart(asyncGraph));
+            graphic.Start();
+
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void quadraticFunc()
+        {
+            double xi = 0;
+            double yi = 0;
+            double xy = 0;
+            double x2 = 0;
+            double x3 = 0;
+            double x4 = 0;
+            double x2y = 0;
+            double yifyi = 0; // сумма (yi - y^) 
+            double yimyavg = 0; // сумма (yi - yсреднее)
+            double yavg; // y среднее
+            double determ;
+            int tableCount = dataGridView1.RowCount - 1;
+            double a, b, c;
+            double det, deta, detb, detc;
+            string exp = String.Empty;
+            string exp_output = String.Empty;
+            
+            PointPairList quadraticList = new PointPairList();
+
+            for (int i = 0; i < tableCount; i++)
+            {
+
+                    xi += Convert.ToDouble(dataGridView1[0, i].Value);
+                    yi += Convert.ToDouble(dataGridView1[1, i].Value);
+                    xy += Convert.ToDouble(dataGridView1[0, i].Value) * Convert.ToDouble(dataGridView1[1, i].Value);
+                    x2 += Math.Pow(Convert.ToDouble(dataGridView1[0, i].Value), 2);
+                    x3 += Math.Pow(Convert.ToDouble(dataGridView1[0, i].Value), 3);
+                    x4 += Math.Pow(Convert.ToDouble(dataGridView1[0, i].Value), 4);
+                    x2y += Math.Pow(Convert.ToDouble(dataGridView1[0, i].Value), 2) * Convert.ToDouble(dataGridView1[1, i].Value);
+                
+            }
+            yavg = yi / tableCount; // y среднее
+
+            det = (x2 * x2 * x2) + (xi * xi * x4) + (x3 * x3 * tableCount) - (tableCount * x2 * x4) - (xi * x2 * x3) - (xi * x3 * x2);
+            deta = (yi * x2 * x2) + (xi * xi * x2y) + (xy * x3 * tableCount) - (x2y * x2 * tableCount) - (xi * x2 * xy) - (yi * xi * x3);
+            a = deta / det;
+            detb = (x2 * x2 * xy) + (yi * xi * x4) + (x3 * x2y * tableCount) - (x4 * xy * tableCount) - (yi * x2 * x3) - (x2 * xi * x2y);
+            b = detb / det;
+            detc = (x2 * x2 * x2y) + (xi * xy * x4) + (x3 * x3 * yi) - (x4 * x2 * yi) - (xi * x2y * x3) - (x2 * xy * x3);
+            c = detc / det;
+            if (b >= 0)
+            {
+                exp += Convert.ToString(a).Replace(',', '.') + "*x^2+" + Convert.ToString(b).Replace(',', '.') + "*x";
+                exp_output += Convert.ToString(Math.Round(a, 3)).Replace(',', '.') + "*x^2+" + Convert.ToString(Math.Round(b, 3)).Replace(',', '.') + "*x";
+                if (c >= 0)
+                {
+                    exp += "+" + Convert.ToString(c).Replace(',', '.');
+                    exp_output += "+" + Convert.ToString(Math.Round(c, 3)).Replace(',', '.');
+                }
+                else
+                {
+                    exp += Convert.ToString(c).Replace(',', '.');
+                    exp_output += Convert.ToString(Math.Round(c, 3)).Replace(',', '.');
+                }
+            }
+            else
+            {
+                exp = Convert.ToString(a).Replace(',', '.') + "*x^2" + Convert.ToString(b).Replace(',', '.') + "*x";
+                exp_output = Convert.ToString(Math.Round(a, 3)).Replace(',', '.') + "*x^2" + Convert.ToString(Math.Round(b, 3)).Replace(',', '.') + "*x";
+                if (c >= 0)
+                {
+                    exp += "+" + Convert.ToString(c).Replace(',', '.');
+                    exp_output += "+" + Convert.ToString(Math.Round(c, 3)).Replace(',', '.');
+                }
+                else
+                {
+                    exp += Convert.ToString(c).Replace(',', '.');
+                    exp_output += Convert.ToString(Math.Round(c, 3)).Replace(',', '.');
+                }
+            }
+
+             
+            
+            for (int i = 0; i < tableCount; i++)  // цикл для нахождения числителя и знаменателя для расчёта коэффициента корреляции
+            {
+                    yifyi += Math.Pow(Convert.ToDouble(dataGridView1[1, i].Value) - func(Convert.ToDouble(dataGridView1[0, i].Value), exp), 2);
+                    yimyavg += Math.Pow(Convert.ToDouble(dataGridView1[1, i].Value) - yavg, 2);
+    
+            }
+
+            determ = Math.Pow(Math.Sqrt(1 - yifyi / yimyavg), 2); // Коэффициент детерминации
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (Convert.ToDouble(dataGridView1[0, i].Value) > maximum)
+                {
+                    maximum = Convert.ToDouble(dataGridView1[0, i].Value);
+                }
+                if (Convert.ToDouble(dataGridView1[0, i].Value) < minimum)
+                {
+                    minimum = Convert.ToDouble(dataGridView1[0, i].Value);
+                }
+            }
+
+            for (int i = Convert.ToInt32(minimum); i <= maximum; i++)
+            {
+                quadraticList.Add(i, func(i, exp));
+            }
+
+            label6.Invoke((MethodInvoker)delegate { label6.Text = "Коэффициент детерминации: " + determ.ToString(); });
+            label4.Invoke((MethodInvoker)delegate { label4.Text = "Функция: " + exp_output; });
+            addPoints(quadraticList, "Квадратичная функция");
+        }
+
+        private void linearFunc()
+        {
+            double xi = 0;
+            double yi = 0;
+            double xy = 0;
+            double x2 = 0;
+            double y2 = 0;
+            double determ; // Коэфициент детерминации
+            int textCount = dataGridView1.RowCount - 1;
+            double a, b;
+            string exp;
+            string exp_output;
+            PointPairList linearList = new PointPairList();
+
+            for (int i = 0; i < textCount; i++)
+            {
+                 xi += Convert.ToDouble(dataGridView1[0, i].Value);
+                 yi += Convert.ToDouble(dataGridView1[1, i].Value);
+                 xy += Convert.ToDouble(dataGridView1[0, i].Value) * Convert.ToDouble(dataGridView1[1, i].Value);
+                 x2 += Math.Pow(Convert.ToDouble(dataGridView1[0, i].Value), 2);
+                 y2 += Math.Pow(Convert.ToDouble(dataGridView1[1, i].Value), 2);
+                
+            }
+            a = ((xi * yi) - textCount * xy) / (Math.Pow(xi, 2) - textCount * x2);
+            b = (xi * xy - x2 * yi) / (Math.Pow(xi, 2) - textCount * x2);
+            determ = Math.Pow((textCount * xy - xi * yi) / Math.Sqrt((textCount * x2 - Math.Pow(xi, 2)) * (textCount * y2 - Math.Pow(yi, 2))), 2); // нахождение коэфициента детерминации
+            if (b >= 0)
+            {
+                exp = Convert.ToString(a).Replace(',', '.') + "*x+" + Convert.ToString(b).Replace(',', '.');
+                exp_output = Convert.ToString(Math.Round(a, 3)).Replace(',', '.') + "*x+" + Convert.ToString(Math.Round(b, 3)).Replace(',', '.');
+            }
+            else
+            {
+                exp = Convert.ToString(a).Replace(',', '.') + "*x" + Convert.ToString(b).Replace(',', '.');
+                exp_output = Convert.ToString(Math.Round(a, 3)).Replace(',', '.') + "*x" + Convert.ToString(Math.Round(b, 3)).Replace(',', '.');
+            }
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (Convert.ToDouble(dataGridView1[0, i].Value) > maximum)
+                {
+                    maximum = Convert.ToDouble(dataGridView1[0, i].Value);
+                }
+                if (Convert.ToDouble(dataGridView1[0, i].Value) < minimum)
+                {
+                    minimum = Convert.ToDouble(dataGridView1[0, i].Value);
+                }
+            }
+
+            for (int i = Convert.ToInt32(minimum); i <= maximum; i++)
+            {
+                
+                linearList.Add(i, func(i, exp));
+            }
+
+            label2.Invoke((MethodInvoker)delegate { label2.Text = "Коэффициент детерминации: " + determ.ToString(); });
+            label3.Invoke((MethodInvoker)delegate { label3.Text = "Функция: " + exp_output; });
+            addPoints(linearList, "Линейная функция");
+
+        }
+
+        private double func(double x, string exp)
+        {
+
+            try
+            {
+                Argument xmain = new Argument("x");
+                Expression y = new Expression(exp.ToLower(), xmain);
+
+                xmain.setArgumentValue(x);
+                return y.calculate();
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+
+        }
+        private void addPoints(PointPairList l, string name)
+        {
+            GraphPane graph = zedGraphControl1.GraphPane;
+
+            if (name == "Линейная функция")
+            {
+                graph.AddCurve(name, l, Color.Blue, SymbolType.None);
+            }
+            else
+            {
+                graph.AddCurve(name, l, Color.Red, SymbolType.None);
+            }
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count != 0) { dataGridView1.Rows.Clear(); }
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            listX.Clear();
+
+            zedGraphControl1.AxisChange();  //Масштабирование и обновление данных об j
+            zedGraphControl1.Invalidate();  //Обновление графика
+        }
+
+        private void сгенерироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(generation));
+            thread.Start();
+
+        }
+
+        private void dataGridViewGeneration()
+        {
+            try
+            {
+                dataGridView1.Invoke((MethodInvoker)delegate { dataGridView1.Rows.Clear(); });
+
+                GraphPane graph = zedGraphControl1.GraphPane;  //Создание системы координат
+                graph.CurveList.Clear();
+
+                if (textBox1.Text == String.Empty)
+                {
+                    MessageBox.Show("Введите кол-во точек для генерации");
+                }
+                else
+                {
+
+                    int n = Convert.ToInt32(textBox1.Text);
+
+                    Random rnd = new Random();
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        dataGridView1.Invoke((MethodInvoker)delegate
+                        {
+                            dataGridView1.Rows.Add();
+                            dataGridView1[0, i].Value = rnd.Next(-50, 50);
+                            dataGridView1[1, i].Value = rnd.Next(-50, 50);
+                        });
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите целочисленное значение");
+            }
+
+        }
+
+        private async void asyncGraph()
+        {
+            await Task.Run(() => linearFunc());
+            await Task.Run(() => quadraticFunc());
+        }
+
+        private async void generation()
+        {
+            await Task.Run(() => dataGridViewGeneration());
+        }
+
+    }
+}
+
